6af89a21ffb2c4d0d4418c3ac13ea3b036c51ca0
Iryna Murzak <iramurzak@gmail.com>
2024-05-15 10:29:08 +0300

    remove console logs

--- src/components/faq.js
+++ src/components/faq.js
@@ -122,9 +122,7 @@ class Faq extends HTMLElement {
     addStructuredData() {
         const faqItems = Array.from(this.querySelectorAll('.accordion')).map((accordion, index) => {
             const question = accordion.querySelector('.question').innerText.trim();
-            console.log(question);
             const answer = accordion.nextElementSibling.querySelector('.panel p').innerText.trim();
-            console.log(answer)
             return {
                 "@type": "Question",
                 "name": question,
